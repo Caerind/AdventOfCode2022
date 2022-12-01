b4efe464bf234f2c76d74b0b096d64214f2ec22c
Caerind <charles.mailly@free.fr>
2022-12-01 11:06:06 +0100

    Update day1.cpp

--- day1.cpp
+++ day1.cpp
@@ -3,7 +3,7 @@
 #include <string>
 #include <fstream>
 
-int main()
+int partOne()
 {
     std::ifstream file("day1.data");
     if (!file)
@@ -32,4 +32,53 @@ int main()
 
     file.close();
     return result;
+}
+
+int partTwo()
+{
+    std::ifstream file("day1.data");
+    if (!file)
+    {
+        std::cerr << "Can't find data" << std::endl;
+        return -1;
+    }
+
+    int max[3] = {0,0,0};
+    int current = 0;
+
+    std::string line;
+    while (std::getline(file, line))
+    {
+        if (line.size() == 0)
+        {
+            if (current > max[0])
+            {
+                max[2] = max[1];
+                max[1] = max[0];
+                max[0] = current;
+            }
+            else if (current > max[1])
+            {
+                max[2] = max[1];
+                max[1] = current;
+            }
+            else if (current > max[2])
+            {
+                max[2] = current;
+            }
+            current = 0;
+        }
+        else
+        {
+            current += atoi(line.c_str());
+        }
+    }
+
+    file.close();
+    return max[0] + max[1] + max[2];
+}
+
+int main()
+{
+    return partTwo();
 }
