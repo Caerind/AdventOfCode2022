ecd75246ca6c7d73f558cd7f6f9b116c32d8eed0
Caerind <charles.mailly@free.fr>
2022-12-04 11:07:06 +0100

    day4

--- AdventOfCode/day4.cpp
+++ AdventOfCode/day4.cpp
@@ -4,7 +4,28 @@ int day4part1(std::ifstream& file)
 {
     int result = 0;
 
-    std::cerr << "NOT IMPLEMENTED" << std::endl;
+    std::string line;
+    while (std::getline(file, line))
+    {
+        std::string firstInput, secondInput, thirdInput, fourthInput;
+        Split(line, firstInput, "-");
+        Split(line, secondInput, ",");
+        Split(line, thirdInput, "-");
+        fourthInput = line;
+        int a = FromString<int>(firstInput);
+        int b = FromString<int>(secondInput);
+        int c = FromString<int>(thirdInput);
+        int d = FromString<int>(fourthInput);
+
+        if (a <= c && d <= b) // cd is contained in ab
+		{
+			result++;
+		}
+        else if (c <= a && b <= d) // ab is contained in cd
+		{
+			result++;
+		}
+    }
 
     return result;
 }
@@ -13,7 +34,28 @@ int day4part2(std::ifstream& file)
 {
     int result = 0;
 
-    std::cerr << "NOT IMPLEMENTED" << std::endl;
+	std::string line;
+	while (std::getline(file, line))
+	{
+		std::string firstInput, secondInput, thirdInput, fourthInput;
+		Split(line, firstInput, "-");
+		Split(line, secondInput, ",");
+		Split(line, thirdInput, "-");
+		fourthInput = line;
+		int a = FromString<int>(firstInput);
+		int b = FromString<int>(secondInput);
+		int c = FromString<int>(thirdInput);
+		int d = FromString<int>(fourthInput);
+
+		if ((a <= c && b >= c) || (a <= d && b >= d)) // cd is overlapping ab
+		{
+			result++;
+		}
+		else if ((c <= a && d >= a) || (c <= b && d >= b)) // ab is overlapping cd
+		{
+			result++;
+		}
+	}
 
     return result;
 }
