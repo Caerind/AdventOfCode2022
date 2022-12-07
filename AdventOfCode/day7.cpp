e1519f2da6eddd2a6f1111917b0aafec7b72b0e3
Caerind <charles.mailly@free.fr>
2022-12-07 10:43:07 +0100

    day7

--- AdventOfCode/day7.cpp
+++ AdventOfCode/day7.cpp
@@ -1,19 +1,126 @@
 #include "aoc.h"
 
+struct Directory
+{
+    std::string name;
+    int parent;
+    std::vector<int> children;
+    int filesSize{ 0 };
+
+    int TotalSize(const std::vector<Directory>& dir) const
+    {
+        int t = filesSize;
+        for (size_t i = 0; i < children.size(); i++)
+        {
+            t += dir[children[i]].TotalSize(dir);
+        }
+        return t;
+    }
+};
+
+bool parse(std::ifstream& file, std::vector<Directory>& dir)
+{
+    int indexCurrent = 0;
+
+    dir.push_back(Directory());
+    dir.back().name = "/";
+    dir.back().parent = -1;
+
+    std::string line;
+    while (std::getline(file, line))
+    {
+        if (Contains(line, "$ cd"))
+        {
+            if (line == "$ cd /")
+            {
+                indexCurrent = 0;
+            }
+            else if (line == "$ cd ..")
+            {
+                indexCurrent = dir[indexCurrent].parent;
+            }
+            else
+            {
+                line = line.substr(5);
+                for (int child : dir[indexCurrent].children)
+                {
+                    if (dir[child].name == line)
+                    {
+                        indexCurrent = child;
+                        break;
+                    }
+                }
+            }
+        }
+        else if (Contains(line, "$ ls"))
+        {
+            // Do nothing
+        }
+        else
+        {
+            if (Contains(line, "dir "))
+            {
+                dir.push_back(Directory());
+                dir.back().name = line.substr(4);
+                dir.back().parent = indexCurrent;
+                dir[indexCurrent].children.push_back((int)dir.size() - 1);
+            }
+            else
+            {
+                std::string sizeStr;
+                Split(line, sizeStr, ' ');
+                dir[indexCurrent].filesSize += FromString<int>(sizeStr);
+            }
+        }
+    }
+    return true;
+}
+
 int day7part1(std::ifstream& file)
 {
     int result = 0;
 
-    std::cerr << "NOT IMPLEMENTED" << std::endl;
+    std::vector<Directory> dir;
+    parse(file, dir);
+    
+    for (auto& d : dir)
+    {
+        if (d.parent != -1) // Exclude root ?
+        {
+            int t = d.TotalSize(dir);
+            if (t <= 100000)
+            {
+                result += t;
+            }
+        }
+    }
 
     return result;
 }
 
 int day7part2(std::ifstream& file)
 {
-    int result = 0;
+    std::vector<Directory> dir;
+    parse(file, dir);
 
-    std::cerr << "NOT IMPLEMENTED" << std::endl;
+    const int totalDiskSpace = 70000000;
+    const int updateSpace = 30000000;
+    int usedSpace = dir[0].TotalSize(dir);
+    int freeSpace = totalDiskSpace - usedSpace;
+    int needToCleanSpace = updateSpace - freeSpace;
 
-    return result;
+    int minSpace = 999999999;
+    for (auto& d : dir)
+    {
+        if (d.parent != -1) // Exclude root here
+        {
+            int t = d.TotalSize(dir);
+            if (t >= needToCleanSpace && t < minSpace)
+            {
+                minSpace = t;
+            }
+        }
+    }
+
+    return minSpace;
 }
